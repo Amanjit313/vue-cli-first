58c2d5278c9652656d7cbe771cea0c25feb097eb
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-12 15:53:10 +0200

    creato un componente e scritto un testo visualizzabile nella pagina

---
+++ src/components/myTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+div{
+  display: flex;
+  justify-content: center;
+}
+
+h1{
+  color: red;
+  border: 1px solid red;
+  padding: 5px;
+}
+
+</style>
